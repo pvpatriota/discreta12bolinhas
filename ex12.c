b4a292847d49bd3799ce295985f02fc44d3e9c2b
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-12-02 18:16:56 -0300

    Adicionado arquivo ex12.c com esqueleto de funcoes basicas.

---
+++ ex12.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <pthreads.h>
+#include <stdlib.h>
+
+void gerar_banco(void);
+void criar_threads(void);
+void criar_estados(void);
+void criar_transicoes(void);
+void gerar_imagem(void);
+
+int main(void)
+{
+    /*TODO: criar bloco base.*/
+    return 0;
+}
+
+void gerar_banco(void)
+{
+    /*TODO: criar funcao para gerar o banco de dados.*/
+    ;
+}
+
+void criar_threads(void)
+{
+    /*TODO: criar funcao para criar threads.*/
+    ;
+}
+
+void criar_estados(void)
+{
+    /*TODO: criar funcao para criar os estados da rede de petri.*/
+    ;
+}
+
+void criar_transicoes(void)
+{
+    /*TODO: criar funcao para criar as transicoes da rede de petri.*/
+    ;
+}
+
+void gerar_imagem(void)
+{
+    /*TODO: criar funcao para gerar imagem da rede de petri.*/
+    ;
+}
