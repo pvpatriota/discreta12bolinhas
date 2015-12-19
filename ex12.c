978b6164655cff696678a26587f961c97979cf3a
Paulo Vitor Alves Patriota <paulovitorpatriota@gmail.com>
2015-12-19 14:40:15 -0300

    Iniciado documentacao doxygen

--- ex12.c
+++ ex12.c
@@ -1,3 +1,33 @@
+/***************************************************************************
+ *   Programa, versão                                                      *
+ *                                                                         *
+ *   Breve descrição do programa                                           *
+ *                                                                         *
+ *   Copyright (C) 2014  Ruben Carlo Benante <rcb [at] beco [dot] cc>      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, read it online                       *
+ *   at <http://www.gnu.org/licenses/>, or write to the                    *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   To contact the author, please write to:                               *
+ *   João Pedro Pacheco Rodrigues Almeida                                  *
+ *   Paulo Vitor Alves Patriota                                            *
+ *   Email: paulovitorpatriota@gmail.com                                   *
+ *   Webpage: http://www.upe.br                                            *
+ ***************************************************************************/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
